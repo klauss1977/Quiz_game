0dbbbc109b1687f84f623182d7b4983cbb26fc29
klauss1977 <klauss1977@yahoo.com>
2023-10-31 00:05:19 +0200

    add Question class and create question_bank

---
+++ main.py
@@ -0,0 +1,10 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for question in question_data:
+    q_text = question["text"]
+    q_answer = question["answer"]
+    new_question = Question(q_text, q_answer)
+    question_bank.append(new_question)
